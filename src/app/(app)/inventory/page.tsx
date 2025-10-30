b15a7e2954184112d6f2f6e12379186a390b3213
Abubakar Mi <abubakarmi131@gmail.com>
2025-10-30 09:15:31 +0000

    we dont need backend for now and also the inventory items should divided

--- src/app/(app)/inventory/page.tsx
+++ src/app/(app)/inventory/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button"
-import { inventoryItems } from "@/lib/data"
+import { inventoryItems, categories } from "@/lib/data"
 import { DataTable } from "@/components/data-table"
 import { columns } from "@/components/inventory/columns"
 import { ItemModal } from "@/components/inventory/item-modal"
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function InventoryPage() {
+  const allCategories = [{ id: 'all', name: 'All' }, ...categories];
+
   return (
     <div className="flex flex-1 flex-col gap-4 md:gap-8">
       <div className="flex items-center justify-between">
@@ -21,7 +24,25 @@ export default function InventoryPage() {
             </ItemModal>
         </div>
       </div>
-      <DataTable columns={columns} data={inventoryItems} />
+      <Tabs defaultValue="All">
+        <TabsList>
+          {allCategories.map(category => (
+            <TabsTrigger key={category.id} value={category.name}>{category.name}</TabsTrigger>
+          ))}
+        </TabsList>
+        {allCategories.map(category => (
+          <TabsContent key={category.id} value={category.name}>
+            <DataTable 
+              columns={columns} 
+              data={
+                category.name === 'All' 
+                  ? inventoryItems 
+                  : inventoryItems.filter(item => item.category === category.name)
+              } 
+            />
+          </TabsContent>
+        ))}
+      </Tabs>
     </div>
   )
 }
